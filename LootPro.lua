4a437e2427ee52bea9b95711d55a50ce29c2d457
wheelbarrel00 <103789673+wheelbarrel00@users.noreply.github.com>
2026-04-11 10:41:49 -0700

    Hotfix: Finalized version to v1.1.0 for full semantic versioning

--- LootPro.lua
+++ LootPro.lua
@@ -2,7 +2,7 @@ local addonName, ns = ...
 
 -- Create the master addon frame and shared namespace
 ns.addon = CreateFrame("Frame", addonName .. "EventFrame")
-ns.addon.VERSION = "1.1"
+ns.addon.VERSION = "1.1.0"
 ns.addon.isTesting = false
 
 -- Create a table specifically for UI Widgets/Helpers
